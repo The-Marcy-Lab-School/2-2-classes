0707f43a5c65525c168209725307114a787cd2a5
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2025-11-04 15:11:17 -0500

    Refactor friends manager factory function
    Refactor makeFriendsManager to accept a username and add printFriends method.

--- 0-factory.js
+++ 0-factory.js
@@ -1,47 +1,58 @@
 // factory functions return objects
-const makeFriendsManager = (...initialFriends) => {
-  const friends = [...initialFriends];
+const makeFriendsManager = (username) => {
+  const friends = [];
 
-  return {
-    getFriends() {
-      return [...friends];
-    },
+  const friendsManager = {
+    username, // we can leave username "public" if we don't care about how it is used or reassigned.
     addFriend(newFriend) {
       if (typeof newFriend !== 'string') {
-        throw Error('new friends must be strings');
-      };
+        console.log('new friends must be strings');
+        return;
+      }
       friends.push(newFriend);
-    }
+    },
+    printFriends() {
+      this.friends.forEach((friend) => {
+        console.log(`${this.username} is friends with ${friend}`);
+      });
+    },
+    // add a getter method to get a copy of the friends array
   }
+  return friendsManager;
 }
 
 // instances of the factory function have their own friends
-const reuben = makeFriendsManager();
+const reuben = makeFriendsManager('reuben');
 reuben.addFriend('carmen')
 reuben.addFriend('ann')
 reuben.addFriend('motun')
-console.log(reuben.getFriends()); // ['carmen', 'ann', 'motun']
+reuben.printFriends();
+// reuben is friends with carmen
+// reuben is friends with ann
+// reuben is friends with motun
 
-
-const maya = makeFriendsManager();
+const maya = makeFriendsManager('maya');
 maya.addFriend('ben')
 maya.addFriend('gonzalo')
-console.log(maya.getFriends()); // ['ben', 'gonzalo']
+reuben.printFriends();
+// maya is friends with ben
+// maya is friends with gonzalo
 
 
-console.log(reuben);
+// console.log(reuben);
 // {
 //   getFriends: [Function: getFriends],
 //   addFriend: [Function: addFriend]
 // }
 
-console.log(maya);
+// console.log(maya);
 // {
 //   getFriends: [Function: getFriends],
 //   addFriend: [Function: addFriend]
 // }
 
 // so, are they the same object?
-console.log(reuben === maya);
+// console.log(reuben === maya);
+
 // are the methods of these objects the same?
-console.log(reuben.addFriend === maya.addFriend);
+// console.log(reuben.addFriend === maya.addFriend);
